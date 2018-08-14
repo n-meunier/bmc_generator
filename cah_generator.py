5d65f77110b849de8abe3e38bf91a8cc1cc77fd3
Nicolas Meunier <NicolasMeunier@MacBook-Pro-de-Nicolas.local>
2018-08-15 00:01:46 +0900

    Added: simple input checker

--- cah_generator.py
+++ cah_generator.py
@@ -1,7 +1,7 @@
 import csv
 import random
 import re
-
+from difflib import SequenceMatcher
 
 def answer_getter(nb_blanks):
     """
@@ -22,6 +22,29 @@ def answer_getter(nb_blanks):
             seen_answers[rand_answer] = 1
     return answers_list
 
+def add_to_list(input, filelist):
+    """"""
+    exist = False
+    _cpt = 0
+    max_ratio = 0
+    max_match = 'nothing'
+    for line in filelist:
+        _cpt += 1
+        ratio = SequenceMatcher(a=line, b=input).ratio()
+        if ratio > max_ratio:
+            max_ratio = ratio
+            max_match = line
+        if ratio > 0.7:
+            exist = True
+            match = line
+            break
+    print('Stopped at %s' % str(_cpt))
+    print('Already exists? %s' % str(exist))
+    if exist:
+        print('Match: %s (%0.2f)' % (str(match), ratio))
+    else:
+        print('No match (Closest: %s with %0.2f), thank you for your input!' %
+              (str(max_match), max_ratio))
 
 blank_pattern = '(____)'
 
@@ -31,10 +54,12 @@ bfile = 'blacklist.csv'
 with open(bfile, 'rb') as f:
     reader = csv.reader(f, delimiter='\n')
     blist = [item for sublist in list(reader) for item in sublist]
+nb_blist = len(blist)
 
 with open(wfile, 'rb') as f:
     reader = csv.reader(f, delimiter='\n')
     wlist = [item for sublist in list(reader) for item in sublist]
+nb_wlist = len(wlist)
 
 sample = random.choice(blist)
 print('Sample: %s (%s)' % (sample, str(type(sample))))
@@ -58,5 +83,8 @@ if match:
         line = line.replace('____', an, 1)
 
     print line
+
+add_to_list('sa bite', wlist)
+add_to_list('toto', wlist)
 
 print(sample)
